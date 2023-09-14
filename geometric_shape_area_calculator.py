ac96c4200dd83208883ba4d410926884f1fc71d7
Ulises_Sanch3z <chino287a@gmail.com>
2023-09-14 19:32:50 -0400

    completed assignment

--- geometric_shape_area_calculator.py
+++ geometric_shape_area_calculator.py
@@ -6,7 +6,7 @@ def main():
     circle_pi = math.pi # DO NOT MODIFY, this line of code is assigning the variable 'circle_pi' equal to Pi ~3.14
 
     # TODO: In terminal, print Welcome to the geometric shape area calculator!
-    
+
     # User Options
     # Circle = 1
     # Rectangle = 2
@@ -14,44 +14,61 @@ def main():
     
     # TODO: Using one print statement, use string concatenation to print the options only 
     # as a single string (make sure to add a space between each option)
+    print("Circle = 1", "Rectangle = 2", "Triangle = 3")
 
     # TODO: In terminal, ask the user "Select a shape by entering 1, 2, or 3' and assign the input to a new variable named 'choice'.
-
+    choice = input("Select a shape by entering 1, 2, or 3")
     # TODO: Convert the variable 'choice' to an integer data type.
-
+    choice = int(choice)
     # TODO: With one line of code, verify the variable 'choice' is indeed the data type integer, use conditional logic and print the output.  If converted correctly, the output in Terminal should read 'True'.
-  
+    if choice is int:
+        print("True")
+
     if choice == 1:  #DO NOT REMOVE THE 'IF'
         # Calculate the area of a circle
 
         # TODO: Assign a new variable named 'radius' and ask for the user's input for the radius of the circle.
+        radius = input("What is the radius of the circle?")
         # TODO: Convert 'radius' to float.
+        radius = float(radius)
         # TODO: Assign a new variable named 'area' and implement the logic to calculate the area of a circle.
+        area = circle_pi * radius ** 2
         # HINT 1 : The formula to find area of a circle: 'circle_pi' times radius squared.  
         # Hint 2 : circle_pi is a variable that has been assigned on Line 9 and equals Pi in math.  
 
     elif choice == 2: # DO NOT REMOVE THE 'ELIF'
         # Calculate the area of a rectangle
         # TODO: Assign new variables 'length' and 'width' and ask for the user's input for the length and width of the rectangle.
+        length = input("What is the length?")
+        width = input("What is the width?")
         # TODO: Convert both 'length' and 'width' to float.
+        length = float(length)
+        width = float(width)
         # TODO: Assign a new variable 'area' and implement the logic to calculate the area of a rectangle.
+        area = length * width
         # HINT: The formula to find the area of a rectangle: length times width
 
     elif choice == 3: #DO NOT REMOVE THE 'ELIF'
         # Calculate the area of a triangle
         # TODO: Assign new variables 'base' and 'height' and ask for the user's input for the base length and height of the triangle.
+        base = input("What is the base of the triangle?")
+        height = input("What is the height of the triangle?")
         # TODO: Convert both 'base' and 'height' to float.
+        base = float(base)
+        height = float(height)
         # TODO: Assign a new variable 'area' and implement the logic to calculate the area of a triangle.
+        area = 0.5 * base * height
         # HINT: The formula to find the area of a Triangle: half times base times height
 
     else:
+        print("Invalid choice .")
         # TODO: If the user enters anything other than 1, 2 or 3, print statement "Invalid choice ."
-    
+
     if choice in [1, 2, 3]: # DO NOT MODIFY
         print(f"The area is: {area:.2f} square units.") # DO NOT MODIFY
 
     # TODO: Print a statement explaining each step required to find and complete your technical assignments.  Be specific. 
-
+    print ("First i went to canvas, selected modules, found w3d4, scrolled down to 'during class', clicked the link for the assignment that redirected me to github, accept assignment on github, refreshed page, copied repo link, opended visual studio, navigated to desired folder with 'cd' command, on termila i used the 'git clone' command followed by the link from github, and then i completed the assignment. Once done i git add the file, git commit -m with a message and finally git push.")
 
 if __name__ == "__main__": # DO NOT MODIFY
     main() # DO NOT MODIFY
